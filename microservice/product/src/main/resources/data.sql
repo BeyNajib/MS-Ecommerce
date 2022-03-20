a973b78fea7b1343074950b149b2591e3ce3a165
BeyNajib <56679862+BeyNajib@users.noreply.github.com>
2022-03-20 15:52:31 +0100

    Modif page html

--- microservice/product/src/main/resources/data.sql
+++ microservice/product/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO Product(id, name, description, illustration, price)
 VALUES (0, 'Ratchet & Clank : Rift Apart', 'Jeux PS5 Action-Plateforme', 'https://urlz.fr/hGz9', 79.99),
-       (1, 'Elden Ring', 'Jeux PS5 Action-RPG', 'https://urlz.fr/hGz7', 69.99),
+       (1, 'Elden Ring', 'Jeux PS5 Action-RPG', 'https://www.micromania.fr/dw/image/v2/BCRB_PRD/on/demandware.static/-/Sites-masterCatalog_Micromania/default/dwc74f0bd9/images/high-res/118422.jpg', 69.99),
        (2, 'Deathloop', 'Jeux PS5 FPS', 'https://urlz.fr/hGzw', 69.99),
        (3, 'Hades', 'Jeux PS5 Rogue-like', 'https://urlz.fr/hGzh', 19.99);
